aecc92b55075586a67dc500aa40fbf532643ef2d
Sebastiao <1952119-tio-valeoescrito-sfchagasfilho@yahoo.com.br.noreply.github.com>
2025-03-24 19:54:37 -0300

    Ratifiquei os arquivos .env e webConfig.js

--- web3Config.js
+++ web3Config.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+const web3 = new Web3('https://mainnet.infura.io/v3/fae945e93b314e3b91d70b1b3982ef1d');
 const contractAbi = [/* Cole o ABI do contrato aqui */];
 const contractAddress = '0xc6da372D3A0aEd587373dc65b03c3F7056614f49';
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
