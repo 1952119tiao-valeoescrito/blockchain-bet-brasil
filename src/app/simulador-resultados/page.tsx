29cce0e7aa4b1e4b04ec9a9972eb2c24648afdb8
Sebastiao <1952119-tio-valeoescrito-sfchagasfilho@yahoo.com.br.noreply.github.com>
2025-07-30 18:49:33 -0300

    feat: adiciona pagina do simulador de resultados

--- src/app/simulador-resultados/page.tsx
+++ src/app/simulador-resultados/page.tsx
@@ -1,6 +1,6 @@
 // src/app/simulador-resultados/page.tsx
 
-import ResultSimulator from '../../components/ResultSimulator';
+import ResultSimulator from '@/components/ResultSimulator';
 
 // Esta é a "página" que o Next.js irá renderizar.
 // Ela simplesmente importa e exibe o seu componente simulador.
